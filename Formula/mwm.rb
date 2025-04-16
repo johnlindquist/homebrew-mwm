7d03bd163c522e24c8479004d2c055c38d5189ce
johnlindquist <johnlindquist@users.noreply.github.com>
2025-04-16 17:23:07 +0000

    Update mwm formula to version v0.3.0

--- Formula/mwm.rb
+++ Formula/mwm.rb
@@ -4,9 +4,9 @@
 class Mwm < Formula
   desc "Powerful macOS command-line tool for window management"
   homepage "https://github.com/johnlindquist/mac-windows-manager"
-  url "https://github.com/johnlindquist/mac-windows-manager/releases/download/v0.2.0/mwm"
-  version "0.2.0"
-  sha256 "b8febf9a5d240e4003c853e5477d3d66d06652c50da01f110397bee488c69389"
+  url "https://github.com/johnlindquist/mac-windows-manager/releases/download/v0.3.0/mwm"
+  version "0.3.0"
+  sha256 "f40cf4193cabc8c2c7f0ec171755b87bb366587d201a1ad7c560df7b155fc80b"
   license "MIT"
 
   def install
